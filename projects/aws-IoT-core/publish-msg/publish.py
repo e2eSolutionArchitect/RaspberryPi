237eb7a68182f438a586735aeadecfba0156b27c
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2024-03-03 16:24:42 -0500

    Update publish.py

--- projects/aws-IoT-core/publish-msg/publish.py
+++ projects/aws-IoT-core/publish-msg/publish.py
@@ -9,7 +9,7 @@ def task():
 myMQTTClient = AWSIoTMQTTClient("MyClientID") #random key, if another connection using the same key is opened the previous one is auto closed by AWS IOT
 myMQTTClient.configureEndpoint("###########.iot.us-east-1.amazonaws.com", 8883)
 
-myMQTTClient.configureCredentials("/home/pi/AWSIoT/root-ca.pem", "/home/pi/AWSIoT/private.pem.key", "/home/pi/AWSIoT/certificate.pem.crt")
+myMQTTClient.configureCredentials("/home/rasbpisom/aws-iot/connect_device_package/root-ca.pem", "/home/rasbpisom/aws-iot/connect_device_package/private.pem.key", "/home/rasbpisom/aws-iot/connect_device_package/certificate.pem.crt")
 
 myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
