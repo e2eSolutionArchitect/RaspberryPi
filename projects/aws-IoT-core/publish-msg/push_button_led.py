e15132eb54f7035c270d0f82797f5940c3c16282
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2024-03-07 20:01:32 -0500

    Update push_button_led.py

--- projects/aws-IoT-core/publish-msg/push_button_led.py
+++ projects/aws-IoT-core/publish-msg/push_button_led.py
@@ -1,7 +1,14 @@
 # To run 'python <filename>' and enter
 # python push_button_led.py
 
-from gpiozero import Button
+from gpiozero import Button, LED
+import time
+
 button = Button(2)
-button.wait_for_press()
-print("Device Activated")
+led  = LED(17)
+
+while True:
+    button.wait_for_press()
+    print("Device has been activated !")
+    led.toggle()
+    time.sleep(0.5)
